fb9064f526c919cadc18c76be57ff8e55fc3f7f9
cutta <cutta@DESKTOP-S11LNA8>
2020-03-05 17:33:35 +0900

    처음등록.

---
+++ src/main/java/com/ggproject/svc/GgprojectApplication.java
@@ -0,0 +1,13 @@
+package com.ggproject.svc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GgprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GgprojectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ggproject/svc/GgprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ggproject.svc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GgprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
